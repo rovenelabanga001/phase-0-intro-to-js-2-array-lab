a3390fbada15b81cc58e4ecb80498f25fc6cdd8b
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-26 16:22:41 +0300

    uses array methods

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  name = [...cats, "Broom"];
+  return name;
+}
+
+function prependCat(name) {
+  name = ["Arnold", ...cats];
+  return name;
+}
+
+function removeLastCat(name){
+    name = cats.slice(0,2)
+    return name;
+}
+
+function removeFirstCat(name){
+    name = cats.slice(1)
+    return name;
+}
+console.log(removeFirstCat())
